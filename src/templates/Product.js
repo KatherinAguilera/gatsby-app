4ab9ded2d084b1b4386169cf2d0d717bea6e378f
Katherine Aguilera <katherinam35@gmail.com>
2019-07-13 13:23:29 -0500

    Manejo de templates

--- src/templates/Product.js
+++ src/templates/Product.js
@@ -1,5 +1,6 @@
+    
 import React from "react"
-
-export default function Product() {
-  return <div>Product</div>
+import { ProductDetail } from "../components"
+export default function Product({ pageContext }) {
+  return <ProductDetail {...pageContext} />
 }
